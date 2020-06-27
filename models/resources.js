d0a6c8c30c019fffa2d6cb0a391e4a6b95781560
Andrew Haberman <a.haberman25@gmail.com>
2020-06-26 20:09:01 -0400

    add resources

--- models/resources.js
+++ models/resources.js
@@ -4,6 +4,22 @@ function getResources() {
     return db("resources")
 }
 
+function findById(id) {
+    return db("project")
+        .where({id})
+        .first();
+}
+
+function addResource(resource) {
+    return db("resources")
+        .insert(resource)
+        .then(id => {
+            return findById(id[0]);
+        });
+}
+
 module.exports = {
+    findById,
     getResources,
+    addResource,
 }
--- routers/resources.js
+++ routers/resources.js
@@ -12,4 +12,16 @@ router.get("/resources", async (req, res, next) => {
     }
 })
 
+router.post('/resource', (req, res) => {
+    const resourceData = req.body;
+
+    resource.addResource(resourceData)
+        .then(resource => {
+        res.status(201).json(resource);
+    })
+        .catch (err => {
+        res.status(500).json({ message: 'Failed to create new resource' });
+    });
+});
+
 module.exports = router
