f2fc64a5eb7373b90dd3a96222b9573527996f28
Elijah Atkins <elijahorama@gmail.com>
2020-08-02 21:30:14 -0600

    added mapper to check body and name has been entered

--- data/projects/mapper.js
+++ data/projects/mapper.js
@@ -1,7 +1,8 @@
 module.exports = {
     intToBoolean,
     booleanToint,
-    projectToBody
+    projectToBody,
+    validateProjectBody
   };
   
   function intToBoolean(int) {
@@ -17,4 +18,15 @@ module.exports = {
       ...recipe,
     };
     return result;
+  }
+
+  function validateProjectBody(req, res, next) {
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ error: "Please provide a Project body" });
+      //look for required fields
+    } else if (!req.body.name) {
+      res.status(400).json({ error: "Please provide unique project name" });
+    } else {
+      next();
+    }
   }
--- data/projects/projectsRouter.js
+++ data/projects/projectsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./projectsModel.js');
+const mapper = require('./mapper')
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
