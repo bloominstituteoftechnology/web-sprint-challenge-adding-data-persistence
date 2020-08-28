ce53156ed2d6673b6e0a9805f3a512e2b0dfb5dd
Evelyn Nguyen <Evelyn@viets-imacx.lan>
2020-08-28 16:13:36 -0700

    setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+
+const server = express()
+const port = process.env.PORT || 5000
+
+
+server.use(express.json())
+
+
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
