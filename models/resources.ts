990012fa88582f7b911f996184ba9ca1bc06e798
ClydeFrog04 <egan.randal@gmail.com>
2020-08-28 18:59:46 -0600

    feat: resource endpoints

--- models/resources.ts
+++ models/resources.ts
@@ -1,7 +1,29 @@
 import {db} from "../data/dbConfig";
 
 
+export function getResources(projectId: string) {
+    return db("project_resources as pr")
+        .join("projects as p", "p.id", "pr.projectId")
+        .join("resources as r", "r.id", "pr.taskId")
+        .where("pr.projectId", projectId)
+        .select("p.name as projectName", "p.description as projectDescription", "r.*");
+}
 
-export function getProjects() {
-    return db("projects");
+export function getResource(id: string) {
+    return db("resources").where({id});
+
+}
+
+//@ts-ignore
+export async function createResource(resource, projectId) {//todo: type task
+    const resExists = await db("resources").where("name", resource.name);
+    if (resExists) {//ensures resources exist one time
+        await db("project_resources").insert({projectId: projectId, resourceId: resExists[0].id});
+        return getResource(resExists[0].id);
+    }
+    const [id] = await db("resources").insert(resource).returning("id");
+    console.log("id ", id);
+    await db("project_resources").insert({projectId: projectId, resourceId: id});
+    return getResource(id);
+
 }
--- routers/resources.ts
+++ routers/resources.ts
@@ -1,15 +1,41 @@
 import express from 'express';
-import * as db from "../models/projects";
+import * as db from "../models/resources";
 
 
 export const resourceRouter = express.Router();
 
 
 //create
-
+resourceRouter.post("/:projectId", async (req, res)=>{
+    try {
+        const newResource = db.createResource(req.body, req.params.projectId);
+        res.status(201).json(newResource);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error creating resource"});
+    }
+});
 
 //read
+resourceRouter.get("/project/:id",  async (req, res) => {
+    try {
+        const projectResources = await db.getResources(req.params.id);
+        res.status(200).json(projectResources);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting project resources"});
+    }
+});
 
+resourceRouter.get("/:id",  async (req, res) => {
+    try {
+        const resource = await db.getResource(req.params.id);
+        res.status(200).json(resource);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting resource"});
+    }
+});
 
 //update
 
