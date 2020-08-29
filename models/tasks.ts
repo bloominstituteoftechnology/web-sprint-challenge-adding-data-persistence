2917d19afd3d58c689fa64880f41717f7a2569ab
ClydeFrog04 <egan.randal@gmail.com>
2020-08-28 18:39:23 -0600

    feat: task routes made

--- models/tasks.ts
+++ models/tasks.ts
@@ -2,7 +2,32 @@ import {db} from "../data/dbConfig";
 
 
 
-export function getProjects() {
-    return db("projects");
+export function getTasks(projectId:string) {
+    return db("project_tasks as pt")
+        .join("projects as p", "p.id", "pt.projectId")
+        .join("tasks as t", "t.id", "pt.taskId")
+        .where("pt.projectId", projectId)
+        .select("p.name as projectName", "p.description as projectDescription", "t.*");
 }
 
+export function getTask(id:string) {
+    return db("tasks").where({id});
+
+}
+
+//@ts-ignore
+export async function createTask(task, projectId) {//todo: type task
+    const [id] = await db("tasks").insert({...task, projectId:projectId}).returning("id");
+    await db("project_tasks").insert({projectId:projectId, taskId:id});
+    return getTask(id);
+
+}
+
+/*
+return db("zoos_animals as za")
+        .join("zoos as z", "z.id", "za.zooId")
+        .join("animals as a", "a.id", "za.animalId")
+        .join("species as s", "s.id", "a.speciesId")
+        .where("za.zooId", zooId)
+        .select("a.id", "a.name", "s.name as species", "za.fromDate", "za.toDate");
+ */
--- routers/tasks.ts
+++ routers/tasks.ts
@@ -1,15 +1,41 @@
 import express from 'express';
-import * as db from "../models/projects";
+import * as db from "../models/tasks";
 
 
 export const taskRouter = express.Router();
 
 
 //create
-
+taskRouter.post("/:projectId", async (req, res)=>{
+    try {
+        const newTask = db.createTask(req.body, req.params.projectId);
+        res.status(201).json(newTask);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error creating task"});
+    }
+});
 
 //read
+taskRouter.get("/project/:id",  async (req, res) => {
+    try {
+        const projectTasks = await db.getTasks(req.params.id);
+        res.status(200).json(projectTasks);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting project tasks"});
+    }
+});
 
+taskRouter.get("/:id",  async (req, res) => {
+    try {
+        const task = await db.getTask(req.params.id);
+        res.status(200).json(task);
+    } catch (e) {
+        console.log(e.stack);
+        res.status(500).json({error: "Error getting task"});
+    }
+});
 
 //update
 
