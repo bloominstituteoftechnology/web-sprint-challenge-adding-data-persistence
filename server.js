e1ab32267a8c0817c71e936a738464dbbbadd29f
Vish Singh <vishalicious213@gmail.com>
2020-08-28 18:57:37 -0700

    building API: GET projects & POST project work (tested in Insomnia)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
+const projectRouter = require('./routers/projectRouter');
 
 server.use(express.json());
+server.use('/projects', projectRouter);
 
 module.exports = server;
