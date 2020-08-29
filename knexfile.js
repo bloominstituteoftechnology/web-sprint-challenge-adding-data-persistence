739c9f321e111a0d3cfc679dc04f7b15c0359f55
Vish Singh <vishalicious213@gmail.com>
2020-08-28 17:53:42 -0700

    added path to migrations files in knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,10 @@ module.exports = {
     connection: {
       filename: './data/entities.db3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    }
   },
 
 //   staging: {
