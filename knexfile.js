8b625fc87732c075a5435c8b44b993eb4e345838
Emmanuel Padilla <manolin923@gmail.com>
2020-11-06 10:48:37 -0800

    not working

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/Projects.db3",
+      filename: "./data/project.db3",
     },
     useNullAsDefault: true,
     migrations: {
