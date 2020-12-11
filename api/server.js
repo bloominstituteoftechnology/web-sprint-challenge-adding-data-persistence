fb2fa63d2ad2f6d750061fb8f2bb414a1ebe0712
Ramsha <ramshanasir.rn@gmail.com>
2020-12-11 11:43:58 -0600

    WIP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 // build your server here
 const express = require('express');
 const ProjectRouter = require('./project/router');
+const ResourceRouter = require('./resource/router');
+const TaskRouter = require('./task/router');
+
 const server = express();
 server.use(express.json());
 server.use('/projects', ProjectRouter);
+server.use('/resources', ResourceRouter);
+server.use('/tasks', TaskRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Server is up and running!' });
