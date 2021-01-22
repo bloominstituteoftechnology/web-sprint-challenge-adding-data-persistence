b0aa626294f80fc3bc9800141f89fd842b7f3792
Ian Luchini <iancarloluchini@gmail.com>
2021-01-22 15:56:53 -0500

    completed taks endpoint and model

--- api/server.js
+++ api/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const server = express();
 const projectRouter = require('./project/router');
 const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
 
 server.use(express.json());
 
 server.use('/api/projects', projectRouter);
 server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
 
 module.exports = server;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,35 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const Task = require('./model');
+const router = express.Router();
+
+// curl -X GET http://localhost:5000/api/tasks
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await Task.getAll();
+        res.status(200).json(tasks);
+    } catch (err) {
+        next(err);
+    }
+})
+
+// curl -d '{"task_description": "this is a description", "task_completed":false, "project_id": 2}' -H 'Content-Type: Application/json' -X POST http://localhost:5000/api/tasks
+router.post('/', async (req, res, next) => {
+    try {
+        const task = await Task.insert(req.body);
+        res.status(201).json(task);
+    } catch (err) {
+        next(err);
+    }
+})
+
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        info: 'something happnede inside the router',
+        message: err.message,
+        stack: err.stack
+    });
+});
+
+module.exports = router;
