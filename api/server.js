2027a9fe3545f6cc20097a3a23d04aaa3ff497ae
e-licea <elicea0412@gmail.com>
2021-02-06 12:43:54 -0500

    created server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here
+const express = require('express');
+const server = express();
+const logger = require('morgan');
+//middlewares
+server.use(logger('short'), express.json());
+
+//server.use middleware
+
+server.get('/', (req, res)=>{ 
+    res.status(200).json({message: 'hey'})
+})
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const PORT = 5001;
+
+server.listen(PORT, ()=>{
+    console.log(`**** SERVER IS RUNNING ON PORT : ${PORT} ***`);
+})
