1ab7f6cd31248f510a5b096716871de2d96e4449
unknown <erickvillegasecheverria@gmail.com>
2021-02-05 20:37:28 -0800

    Added routes to server

--- api/server.js
+++ api/server.js
@@ -3,7 +3,14 @@ const express = require("express");
 const server = express();
 const helmet = require("helmet");
 
+const projectRouter = require("./project/router");
+const resourceRouter = require("./resource/router");
+const taskRouter = require("./task/router");
+
 server.use(express.json());
 server.use(helmet());
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
+server.use("/api/tasks", taskRouter);
 
 module.exports = server;
