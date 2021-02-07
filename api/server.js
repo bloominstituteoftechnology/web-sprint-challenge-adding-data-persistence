c9ca84bffb941f5287de8365159367b6e5940859
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-02-07 11:23:00 -0800

    Updated the index,server and dbConfig files

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
 // build your server here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projectRouter = require('./project/project-router.js'); //need to create router file
+
+const resourceRouter = require('./resource/resource-router.js'); //need to create router file
+
+const taskRouter = require('./task/task-router.js'); //need to create router file
+
+
+server.use("api/project", projectRouter)
+
+server.use("api/resource", resourceRouter)
+
+server.use("api/task", taskRouter)
+
+
+
+module.exports = server
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,2 +1,10 @@
 // Complete your db configuration using the `environment` variable.
 const environment = process.env.NODE_ENV || "development";
+
+const knex = require('knex');
+
+const config = require('../knexfile.js');
+
+const configuredKnex = knex(config[environment]);
+ 
+module.exports = configuredKnex;
