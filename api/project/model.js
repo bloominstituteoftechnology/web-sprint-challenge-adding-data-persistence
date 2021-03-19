d2eb8edcfe1d3e0803a84a74aa40e02f04acf8ea
ty <tylerpmatthews1998@gmail.com>
2021-03-19 16:11:47 -0400

    Codegrade fix

--- api/project/model.js
+++ api/project/model.js
@@ -1,8 +1,9 @@
 const db = require("../../data/dbConfig");
+//get all projects
 const getAllProjects = () => {
   return db("projects");
 };
-
+//post a project
 const postProject = (project) => {
   return db("projects").insert(project);
 };
