2b5e920e748cf83c83b8553891ad9d0d4d787dcc
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-03-19 10:17:59 -0600

    Adds basic routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,11 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: 'something went wrong inside the PROJECTS router',
+      errMessage: err.message,
+    })
+})
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,11 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: 'something went wrong inside the RESOURCES router',
+      errMessage: err.message,
+    })
+})
+
+module.exports = router;
