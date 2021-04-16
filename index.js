f1f6eddcbb0a3436dbe6a3ece4484986dacdca96
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-16 08:39:35 -0400

    server is up and running, dependencies added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 6000;
+
+server.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}...`)
+})
