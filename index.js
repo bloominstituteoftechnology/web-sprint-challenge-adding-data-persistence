d1ccafb9ede41689061f22ab79683cac6479a5b1
daniel-vazquez <da.vazquez728@gmail.com>
2021-05-01 17:34:25 -0700

    codegrade setup and installed dependencies

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // start your server here
+
+const server = require("./server")
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
