99dda7639203880cdb65c680feed0695c5e32765
Anwar <AnwarAldogmi@gmail.com>
2021-06-11 11:30:42 -0700

    added deps & scripts

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const port = process.env.PORT
+
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
