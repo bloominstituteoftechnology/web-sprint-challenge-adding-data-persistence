69e103dcf23f70f4838db67133befbd37073760e
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-06-11 14:36:59 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -3,19 +3,28 @@
 //setting up
 const express = require('express');
 const server = express();
-server.use(express.json);
+const helmet = require("helmet");
+server.use(express.json());
+server.use(helmet());
 
 //routers
 const projectRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
+const resourceRouter = require('./resource/router.js');
 const taskRouter = require('./task/router');
 
-server.use(projectRouter);
-server.use(resourceRouter);
-server.use(taskRouter);
+server.use("/api/projects",projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks',taskRouter);
+
+//is server working?
+server.get('/',(req,res)=>{
+    res.json({message: 'hello'})
+});
+
 
 //error handling MW
-server.use((err,req,res,next)=>{
+server.use((err, req, res, next)=>{
+    
     console.log(err);
     res.status(500).json({
         message: "Something went wrong"
