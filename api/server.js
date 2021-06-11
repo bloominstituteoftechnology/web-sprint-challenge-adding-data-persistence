30230ba0e719b7184dc2f144a55016e3648f9d79
Anwar <AnwarAldogmi@gmail.com>
2021-06-11 12:06:55 -0700

    updated server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,27 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
+
+server.use('*', (req, res) => {
+    res.send('yoyo')
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = server
