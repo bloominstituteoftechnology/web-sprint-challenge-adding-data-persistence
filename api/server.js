8816408095d440f0e0aac7c62cac65dc96052523
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-25 20:01:00 -0700

    Code changes to server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require('helmet');
+const projectsRouter = require('./project/router.js')
+const resourcesRouter = require('./resource/router.js')
+const tasksRouter = require('./task/router.js')
+
+const server = express()
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+
+
+server.get("/", (req, res) => {
+    res.status(200).json( "API is running" )
+  })
+
+module.exports = server
