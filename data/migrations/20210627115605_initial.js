c632e39cf334e81e481d53d302e766f9d6778aa0
Polly Yospan <porgroovy@gmail.com>
2021-06-27 07:57:02 -0400

    create migrate

---
+++ data/migrations/20210627115605_initial.js
@@ -0,0 +1,53 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("projects", (table) => {
+    table.increments("project_id");
+    table.text("project_name").notNullable();
+    table.text("project_description");
+    table.boolean("project_completed").defaultTo(false);
+  });
+
+  await knex.schema.createTable("resources", (table) => {
+    table.increments("resource_id");
+    table.text("resource_name").notNullable().unique();
+    table.text("resource_description");
+  });
+
+  await knex.schema.createTable("tasks", (table) => {
+    table.increments("task_id");
+    table.text("task_description").notNullable();
+    table.text("task_notes");
+    table.boolean("task_completed").defaultTo(false);
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("project_id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+
+  await knex.schema.createTable("project_resources", (table) => {
+    table.increments("project_resources_id");
+    table
+      .integer("resource_id")
+      .notNullable()
+      .references("resource_id")
+      .inTable("resources")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("project_id")
+      .notNullable()
+      .references("project_id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("project_resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
+};
