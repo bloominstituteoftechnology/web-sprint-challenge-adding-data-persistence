f8b1d3391f5294cb2a9ff713c1d096d28aff879d
Polly Yospan <porgroovy@gmail.com>
2021-06-27 07:48:05 -0400

    finish all model.js

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,16 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+async function find() {
+  return await db("resources");
+}
+
+async function add(resource) {
+  const newResource = await db("resources").insert(resource, "id");
+  return db("resources").where("resource_id", newResource);
+}
+
+module.exports = {
+  find,
+  add,
+};
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,39 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig");
+
+function find() {
+  return db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select(
+      "task_id",
+      "task_description",
+      "task_notes",
+      "task_completed",
+      "project_name",
+      "project_description"
+    );
+}
+
+async function add(task) {
+  const newTask = await db("tasks").insert(task, "id");
+  return await db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .where("t.task_id", newTask)
+    .select(
+      "task_id",
+      "task_description",
+      "task_notes",
+      "task_completed",
+      "p.project_id"
+    );
+}
+
+function getById(id) {
+  return db("recipes").where({ id: Number(id) });
+}
+
+module.exports = {
+  find,
+  add,
+  getById,
+};
