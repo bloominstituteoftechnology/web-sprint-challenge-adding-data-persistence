0ecd410dce81c613130bfd514fc24e2c32572edc
Polly Yospan <porgroovy@gmail.com>
2021-06-27 06:54:49 -0400

    finish project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,29 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+async function find() {
+  const data = await db("projects");
+  const output = data.map((e) => {
+    if (e.project_completed === 0) {
+      return { ...e, project_completed: false };
+    } else if (e.project_completed === 1) {
+      return { ...e, project_completed: true };
+    }
+  });
+  return output;
+}
+
+async function add(project) {
+  const newProject = await db("projects").insert(project, "id");
+  return db("projects").where("project_id", newProject);
+}
+
+function getById(project_id) {
+  return db("projects").where({ project_id: Number(project_id) });
+}
+
+module.exports = {
+  find,
+  add,
+  getById,
+};
