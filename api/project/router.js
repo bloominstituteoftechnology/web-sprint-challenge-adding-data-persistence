e92767fc12d68cf6ab74f817e59a147dbbb346f7
Polly Yospan <porgroovy@gmail.com>
2021-06-27 07:51:10 -0400

    finish all router.js

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,46 @@
 // build your `/api/projects` router here
+const express = require("express");
+const router = express.Router();
+const db = require("./model");
+
+router.get("/", async (req, res) => {
+  try {
+    const projects = await db.find();
+    res.json(projects);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The projects information could not be retrieved" });
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const project = req.body;
+
+  try {
+    const newProject = await db.add(project);
+    const output = {
+      project_id: newProject[0].project_id,
+      project_name: newProject[0].project_name,
+      project_description: newProject[0].project_description,
+      project_completed: newProject[0].project_completed === 0 ? false : true,
+    };
+    res.status(201).json(output);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:project_id", async (req, res, next) => {
+  const { project_id } = req.params;
+  console.log(req.query);
+
+  try {
+    const recipe = db.getById(project_id);
+    res.status(200).json(recipe);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,34 @@
 // build your `/api/resources` router here
+const express = require("express");
+const router = express.Router();
+const db = require("./model");
+
+router.get("/", async (req, res) => {
+  try {
+    const resources = await db.find();
+    res.json(resources);
+  } catch (err) {
+    res.status(500).json({ message: "The resources could not be retrieved" });
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const resource = req.body;
+
+  try {
+    const check = await db.find();
+    check.map((e) => {
+      if (resource.resource_name === e.resource_name) {
+        res
+          .status(404)
+          .json({ message: "this resource_name is already exist" });
+      }
+    });
+    const newResource = await db.add(resource);
+    res.status(201).json(newResource[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
