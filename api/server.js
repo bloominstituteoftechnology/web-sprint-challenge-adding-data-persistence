f6b2b819c87f4bc485da0fe265d2e5a16db27da7
Polly Yospan <porgroovy@gmail.com>
2021-06-27 06:42:43 -0400

    install dependencies, finish index.js, finish server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,26 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const projectRouter = require("./project/router");
+const resourceRouter = require("./resource/router");
+const taskRouter = require("./task/router");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
+server.use("/api/tasks", taskRouter);
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
