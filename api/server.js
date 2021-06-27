e0d5ee1358641a74e6ac0140660ee24458b46a37
miketillman95 <matillman95@gmail.com>
2021-06-27 16:00:10 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require('express');
+const router = require('./project/router');
+const server = express();
+
+server.use(express.json());
+server.use('/api', router)
+
+server.use((err, req, res, next )=> { // eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack:err.stack,
+    })
+})
+
+module.exports = server 
