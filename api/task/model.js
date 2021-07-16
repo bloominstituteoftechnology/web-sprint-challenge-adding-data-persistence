5624d47d1d037b9a932e01f88678890d44f81da5
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-16 13:48:02 -0500

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,17 @@
 const db = require('../../data/dbConfig')
 
-function getTasks() {
-    return db('tasks')
+async function getTasks() {
+    const tasks = await db('tasks as t')
+        .leftJoin('projects as p', 'p.project_id', 't.project_id')
+        .select(
+            't.task_id', 
+            't.task_description', 
+            't.task_notes', 
+            't.task_completed', 
+            'p.project_name', 
+            'p.project_description'
+            )
+    return tasks
 }
 
 function addTask(task) {
