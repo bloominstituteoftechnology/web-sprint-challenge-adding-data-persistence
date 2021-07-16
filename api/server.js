757fb5db74932098e318b94a4252f5fb01449b00
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-16 08:24:19 -0500

    installs/scripts/index.js/server.js - installed appropriate dependencies, added scripts to package.json file, built out scaffolding for index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+// import router(s)
+
+const server = express();
+
+server.use(express.json());
+// plug in appropriate router
+// server.use('/api/schemes', SchemeRouter);
+
+server.use('*', (req, res, next) => {
+    next({status: 404,
+        message: 'not found'
+    })
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server.js');
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
