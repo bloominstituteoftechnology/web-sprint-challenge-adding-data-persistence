0e7bfbad0ff76495cf76fe4cba0299e6dc458316
Richino <archerrichino@gmail.com>
2021-08-19 20:43:32 -0400

    finished

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,29 @@
 // build your `/api/tasks` router here
+const express = require("express");
+const Tasks = require("./model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+	await Tasks.getTasks()
+		.then((tasks) => {
+			res.status(200).json(tasks);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "There was an error retrieving tasks" });
+		});
+});
+
+router.post("/", (req, res) => {
+	Tasks.addTask(req.body)
+		.then((task) => {
+			res.status(201).json(task);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "Error adding task" });
+		});
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // start your server here
+const server = require("./api/server");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`running on port ${port}`));
