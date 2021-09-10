cb7e45a77bfb2b8034b3dfec19ad596e1ae0869d
ferengitooth <ehitsamill@gmail.com>
2021-09-10 11:47:29 -0700

    resource midel and router wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,16 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const getResources = () => {
+    return db('resources')
+}
+
+async function addResources(resource) {
+    const newResource = await db("resources").insert(resource, "id");
+    return db("resources").where("resource_id", newResource);
+}
+
+module.exports = {
+    getResources,
+    addResources
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,36 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+const Resource = require('./model')
+
+router.get('/', (req, res) => {
+    Resource.getResources()
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(err => {
+            err.status(500).json({
+                message: err.message
+            })
+        }) 
+})
+
+router.post('/', async (req,res, next) => {
+    const resource = req.body
+    try {
+        const check = await Resource.getResources()
+        check.map((e) => {
+            if (resource.resource_name === e.resource_name) {
+                res.status(404).json({
+                    message: 'Name is already taken'
+                })
+            }
+        })
+
+        const newResource = await Resource.addResources(resource)
+        res.status(201).json(newResource[0])
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
