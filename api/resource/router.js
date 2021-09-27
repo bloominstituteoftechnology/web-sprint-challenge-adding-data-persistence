50ce3f8eac938eedac2adef027a7722afefadf87
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-09-27 00:22:28 -0700

    update resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -5,12 +5,28 @@ const Resources = require('./model.js');
 
 const router = express.Router();
 
-router.get('/', (_, res, next) => {
+router.get('/', (req, res, next) => {
     Resources.getAll()
         .then(resources => {
             res.status(200).json(resources)
         })
         .catch(err => next(err))
 })
+
+router.post('/', (req,res,next)=>{
+    const newResource = req.body
+    Resources.createProject(newResource)
+    .then(resource=>{
+        res.status(200).json(resource)
+    })
+    .catch(err => next(err))
+})
+
+router.use((err, req, res) => { 
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
 
 module.exports = router;
