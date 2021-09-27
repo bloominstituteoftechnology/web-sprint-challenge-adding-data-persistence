97d92bf33a784d07dfb79340c3057a506c81bef4
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-09-26 23:43:57 -0700

    api/projects flushed out

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,43 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const booleanConvert = project => {
+    if (project.project_completed === 0 || !project.project_completed) {
+        return {...project, 'project_completed': false};
+    } else {
+        return {...project, 'project_completed': true};
+    }
+};
+
+async function getById(id){
+    const newProject = await db('project as p')
+        .where('p.project')
+        .first()
+    return booleanConvert(newProject)
+}
+
+async function getAll() {
+    const projects = await db('projects as p');
+    const projectsBoolean =  projects.map(project => {
+        return booleanConvert(project);
+    });
+    return projectsBoolean;
+}
+
+async function createProject() {
+    const booleanConvert = project => {
+        if (project.project_completed === false || !project.project_completed) {
+            return {...project, 'project_completed': 0}
+        } else {
+            return {...project, 'project_completed': 1}
+        }
+    }
+    const [project_id] = await db('projects').insert(booleanConvert(project))
+    return getById(project_id)
+}
+
+module.exports = {
+    getAll,
+    getById,
+    createProject,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -5,4 +5,28 @@ const Projects = require('./model.js');
 
 const router = express.Router();
 
+router.get('/', (req,res,next) => {
+    Projects.getAll()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => next(err))
+});
+
+router.get('/:id', (req,res,next) =>{
+    Projects.getById(req.params.id)
+        .then(project =>{
+            res.status(200).json(project)
+        })
+        .catch(err => next(err))
+})
+
+router.post('/', (req,res,next) =>{
+    Projects.createProject(req.body)
+        .then(newProject =>{
+            res.status(201).json(newProject)
+        })
+        .catch(err => next(err))
+});
+
 module.exports = router;
