773b014d2999432d8ce68cd90b718c8220d4d4af
Raj-04 <1204patel.raj@gmail.com>
2021-11-05 13:27:40 -0400

    Added Columns to project table

--- data/migrations/20211105170953_first-migration.js
+++ data/migrations/20211105170953_first-migration.js
@@ -3,6 +3,11 @@ exports.up = async function(knex) {
     await knex.schema
         .createTable('projects', (table) => {
             table.increments('project_id')
+            table.string('project_name')
+                .notNullable()
+            table.string('project_description')
+            table.boolean('project_completed')
+                .defaultTo(false)
         })
         .createTable('resources', (table) => {
             table.increments('resource_id')
