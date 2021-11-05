45fb719c9821d6284208e70d96d4a78380b90cb2
Raj-04 <1204patel.raj@gmail.com>
2021-11-05 13:33:44 -0400

    Added columns to the resources table

--- data/migrations/20211105170953_first-migration.js
+++ data/migrations/20211105170953_first-migration.js
@@ -3,14 +3,18 @@ exports.up = async function(knex) {
     await knex.schema
         .createTable('projects', (table) => {
             table.increments('project_id')
-            table.string('project_name')
+            table.string('project_name', 128)
                 .notNullable()
-            table.string('project_description')
+            table.string('project_description', 255)
             table.boolean('project_completed')
                 .defaultTo(false)
         })
         .createTable('resources', (table) => {
             table.increments('resource_id')
+            table.string('resource_name', 128)
+                .notNullable()
+                .unique()
+            table.string('resource_description', 255)
         })
         .createTable('tasks', (table) => {
             table.increments('task_id')
