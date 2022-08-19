cf468728705da64bfbe3e40ded293e2b7b4b78b6
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-18 23:53:17 -0400

    start and server scripts added

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,47 @@
-// do not make changes to this file
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-}
+// Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
+
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/database.db3' },
-    seeds: { directory: './data/seeds' },
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/test.db3' },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
