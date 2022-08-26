9bf81036cb57fd87f922a5498bf4db02f107c30d
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-26 12:53:21 -0400

    created projects table

--- data/migrations/20220826162044_create_dbtables.js
+++ data/migrations/20220826162044_create_dbtables.js
@@ -1,15 +1,29 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function(knex) {
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments('project_id');
+        tbl.string('project_name').notNullable();
+        tbl.string('project_description');
+        tbl.boolean('project_completed').defaultTo('false');
+    });
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
   
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.down = function(knex) {
   
 };
