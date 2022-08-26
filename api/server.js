322a1b59326f4423f7d303cfa887e81fa4c71f65
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-26 12:06:10 -0400

    project router established

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+ const express = ('express');
+ const server = express();
+
+ server.use(express.json());
+
+
+ const projectRouter = require('./project/router');
+ server.use('/api/project', projectRouter)
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server');
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-    console.log(`Locked && Loaded on PORT ${PORT}`)
-})
+    console.log(`Locked && Loaded on PORT ${PORT}`);
+});
