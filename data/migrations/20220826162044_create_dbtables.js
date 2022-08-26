8b5a79cdd96ea92559641cbc76f9b6071c284080
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-26 19:49:13 -0400

    migration to latest successful

--- data/migrations/20220826162044_create_dbtables.js
+++ data/migrations/20220826162044_create_dbtables.js
@@ -1,30 +1,30 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable('projects', tbl => {
-        tbl.increments('project_id');
-        tbl.string('project_name').notNullable();
-        tbl.string('project_description');
-        tbl.boolean('project_completed').defaultTo('false');
+        tbl.increments('project_id')
+        tbl.varchar('project_name', 100).notNullable()
+        tbl.string('project_description')
+        tbl.boolean('project_completed').defaultTo('false')
     })
 
         .createTable('resources', tbl => {
-            tbl.increments('resources_id');
-            tbl.string('resource_name').notNullable().unique();
-            tbl.string('resource_description');
+            tbl.increments('resources_id')
+            tbl.varchar('resource_name').notNullable().unique()
+            tbl.string('resource_description')
         })
 
         .createTable('tasks', tbl => {
-            tbl.increments('task_id');
-            tbl.string('task_description').notnullable();
-            tbl.string('task_notes');
-            tbl.boolean('task_completed').defaultTo('false');
-            tbl.integer('project_id').notNullable().references('project_id').inTable('projects');
+            tbl.increments('task_id')
+            tbl.varchar('task_description').notNullable()
+            tbl.string('task_notes')
+            tbl.boolean('task_completed').defaultTo('false')
+            tbl.integer('project_id').notNullable().references('project_id').inTable('projects')
         })
 
         .createTable('project_resources', tbl => {
-            tbl.increments('joined_id');
-            tbl.integer('project_id').references('project_id').inTable('projects');
-            tbl.integer('resource_id').references('resource_id').inTable('resources');
+            tbl.increments('joined_id')
+            tbl.integer('project_id').references('project_id').inTable('projects')
+            tbl.integer('resource_id').references('resource_id').inTable('resources')
         })
 
 
