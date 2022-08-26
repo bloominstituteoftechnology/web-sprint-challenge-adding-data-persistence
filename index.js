5f49978781ed3c39cd460079a1c34948fc04fae9
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-26 12:02:13 -0400

    build ser initialization

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require('dotenv').config();
+const server = require('./api/server');
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+    console.log(`Locked && Loaded on PORT ${PORT}`)
+})
