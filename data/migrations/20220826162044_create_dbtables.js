5a2c84635e728b6be4da455868c7aa23c2bba86c
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-26 13:16:33 -0400

    tasks tasble completed

--- data/migrations/20220826162044_create_dbtables.js
+++ data/migrations/20220826162044_create_dbtables.js
@@ -1,5 +1,5 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('projects', tbl => {
         tbl.increments('project_id');
         tbl.string('project_name').notNullable();
@@ -7,27 +7,27 @@ exports.up = function(knex) {
         tbl.boolean('project_completed').defaultTo('false');
     })
 
-   .createTable('resources', tbl => {
-       tbl.increments('resources_id');
-       tbl.string('resource_name').notNullable().unique();
-       tbl.string('resource_description');
-   })
+        .createTable('resources', tbl => {
+            tbl.increments('resources_id');
+            tbl.string('resource_name').notNullable().unique();
+            tbl.string('resource_description');
+        })
+
+        .createTable('tasks', tbl => {
+            tbl.increments('task_id');
+            tbl.string('task_description').notnullable();
+            tbl.string('task_notes');
+            tbl.boolean('task_completed').defaultTo('false');
+            tbl.integer('project_id').notNullable().references('project_id').inTable('projects');
+        })
 
 
 
 
 
-
-
-
-
-
-
-
-  
 };
 
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
